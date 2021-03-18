53f0061cd18e1cabe076392440adefa5c1aa4271
garche <archersve@gmail.com>
2021-03-18 20:46:09 +0500

    task 1-4

--- src/task_2/index.js
+++ src/task_2/index.js
@@ -1,4 +1,4 @@
-const { Time } = require('../task_1/index');
+let { Time } = require('../task_1/index.js');
 /** Задача 2 - Класс Meeting
 Требуется написать класс встречи - Meeting, который содержит:
 	2.1. Поле c датой встречи (объект класса Date)
@@ -17,6 +17,25 @@ const { Time } = require('../task_1/index');
 @param {Time} startTime - Время начала встречи
 @param {Time} endTime - Время конца встречи
  */
-function Meeting(meetingDate, startTime, endTime) { };
+function Meeting(meetingDate, startTime, endTime)
+{
+	if(!startTime || !endTime || startTime.hours > 19 || startTime.hours < 8 || 
+		startTime.minutes > 59 || startTime.minutes < 0 ||
+		(startTime.minutes+startTime.hours*60) > (endTime.minutes+endTime.hours*60) ||
+		endTime.hours > 19 || endTime.hours < 8 || endTime.minutes > 59 || endTime.minutes < 0)
+		{
+			throw new Error();
+		}
+	
+	this.meetingDate = meetingDate;
+	this.startTime = startTime;
+	this.endTime = endTime;
+
+	
+	Meeting.prototype.isMeetingInTimeRange = function(start, end)
+	{
+		return !(start.isLater(endTime) || end.isEarlier(startTime));
+	}
+}
 
 module.exports.Meeting = Meeting;
