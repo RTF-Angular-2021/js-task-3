c6321a6e4018757998336e09251254e7724f5a6f
diukin9 <petr.diukin@urfu.me>
2021-05-23 18:13:16 +0500

    Петр Дюкин // js-task-3

--- src/task_1/index.js
+++ src/task_1/index.js
@@ -1,20 +1,16 @@
-/** Задача 1 - Класс Time
-Требуется написать класс времени - Time, который содержит:
-	1.1. Поле с часами — hours (number)
-	1.2. Поле с минутами — minutes (number)
-	1.3. Прототип класса должен содержать метод сравнения isEarlier,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится позже того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-	1.4. Прототип класса должен содержать метод сравнения isLater,
-	который принимает объект класса Time и возвращает true,
-	если переденное значение времени находится раньше того,
-	которое содержится в экземпляре объекта, у которого вызван метод.
-@constructor
-@this {Time}
-@param {number} hours - Час
-@param {number} minutes - Минуты
- */
-function Time(hours, minutes) { };
+function Time(hours, minutes) {
+	if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59)
+		throw "Неверный формат времени";
+	this.hours = hours;
+	this.minutes = minutes;
+}
+
+Time.prototype.isEarlier = function (obj) {
+	return this.hours < obj.hours || this.hours === obj.hours && this.minutes < obj.minutes;
+}
+
+Time.prototype.isLater = function (obj) {
+	return this.hours > obj.hours || this.hours === obj.hours && this.minutes > obj.minutes;
+}
 
 module.exports.Time = Time;
--- src/task_5/index.js
+++ src/task_5/index.js
@@ -1,18 +1,25 @@
 const { Organaizer } = require('../task_4/index');
 
-/* Задача 5 - Расширить прототип класса Organaizer следующими методами:
-	5.1. addMeeting, принимающий — объект класса Meeting.
-	Результатом работы должно быть true и добавление объекта встречи в массив встреч,
-	если встреча успешно добавлена в органайзер и false в ином случае.
-	Встреча может быть добавлена, если:
-		В день встречи в органайзере нет отпуска
-		Время встречи не пересекается с какой-нибудь другой встречей в органайзере
-	5.2. addVacation, принимающий — объект класса Vacation.
-	Результатом работы должно быть true и добавление объекта отпуска в массив отпусков,
-	если отпуск успешно добавлена в органайзер и false в ином случае.
-	Отпуск может быть добавлен, если:
-		Отпуск не попадает в промежуток другого отпуска
-		В промежуток отпуска не назначено никаких встреч
- */
+Organaizer.prototype.addMeeting = function (meeting) {
+    for (let x of this.vacations)
+        if (x.isDateInVacation(meeting.meetingDate)) return false;
+    for (let x of this.meetings)
+        if (meeting.isMeetingInTimeRange(x.startTime, x.endTime) &&
+            x.meetingDate.getDate() === meeting.meetingDate.getDate())
+            return false;
+    this.meetings.push(meeting);
+    return true;
+}
+
+Organaizer.prototype.addVacation = function (vacation) {
+    for (let x of this.vacations)
+        if (x.isDateInVacation(vacation.vacationStartDate) ||
+            x.isDateInVacation(vacation.vacationEndDate))
+            return false;
+    for (let x of this.meetings)
+        if (vacation.isDateInVacation(x.meetingDate)) return false;
+    this.vacations.push(vacation);
+    return true;
+}
 
 module.exports.Organaizer = Organaizer;
